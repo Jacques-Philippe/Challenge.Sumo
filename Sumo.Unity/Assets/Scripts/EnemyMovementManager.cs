b378bcdff0fdc735903c2c3f506865e1331728ae
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2022-10-16 12:11:56 -0400

    Enemy chases player

---
+++ Sumo.Unity/Assets/Scripts/EnemyMovementManager.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EnemyMovementManager : MonoBehaviour
+{
+    private PlayerDeath playerDeath;
+    private GameObject player;
+
+    private Rigidbody rigidBody;
+
+    private Action movementScheme;
+
+    public float speed;
+
+    private void Awake()
+    {
+        this.playerDeath = GameObject.FindObjectOfType<PlayerDeath>();
+        this.player = playerDeath.gameObject;
+
+        this.rigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        if (this.player != null)
+        {
+            movementScheme = MoveTowardsPlayer;
+        }
+        else
+        {
+            movementScheme = CircleArena;
+        }
+    }
+
+    private void OnEnable()
+    {
+        this.playerDeath.OnPlayerDeath += SwitchToCircleArenaMovementScheme;
+    }
+
+    private void Update()
+    {
+        this.movementScheme.Invoke();
+    }
+
+    void SwitchToCircleArenaMovementScheme()
+    {
+        this.movementScheme = CircleArena;
+    }
+
+    void MoveTowardsPlayer()
+    {
+        Vector3 playerPosition = this.player.transform.position;
+        Vector3 direction = playerPosition - this.transform.position;
+
+        this.rigidBody.AddForce(direction.normalized * speed * Time.deltaTime, ForceMode.Impulse);
+    }
+
+    void CircleArena()
+    {
+        Debug.Log("Circling arena");
+    }
+}
